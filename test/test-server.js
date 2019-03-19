58f20c0c3f54ae64ca492c1e07c3dbed245f2169
Rebecca Bob <rebecca.rupel@gmail.com>
2019-03-19 11:25:22 -0700

    added travis, fixed bugs in server

--- test/test-server.js
+++ test/test-server.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../server.js");
 
-const should = chai.should();
+const expect = chai.expect;
+
 chai.use(chaiHttp);
 
-
-describe('GET endpoint', function () {
-
-    it('should return status 200', function () {
-      let res;
-      return chai.request()
-        .get('/')
-        .then(
-          res.should.have.status(200)
-        )
-    })
+describe("index page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get("/")
+      .then(function(res) {
+        expect(res).to.have.status(200);
+      });
+  });
 });
