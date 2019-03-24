6017bfee3a09e1a29e7775162cf5746370870a3e
Rebecca Bob <rebecca.rupel@gmail.com>
2019-03-23 20:28:32 -0700

    added datasentence

---
+++ public/datasentence.js
@@ -0,0 +1,3 @@
+const WARM = ["Nothing happens.", "There's movement inside the egg!"];
+
+const TALK = ["Nothing happens. What did you expect?", "You think you hear a noise from inside the egg. Was it your imagination?"];
