c996d029cb69568a08182ff4eea48a6748121c0c
Matthew Prestifilippo <matthew.prestifilippo@syapse.com>
2019-04-02 16:28:20 -0700

    Fix delete feature

--- public/pets.js
+++ public/pets.js
@@ -2,26 +2,35 @@ function fetchPets() {
   fetch('/pets')
     .then(res => res.json())
     .then(json => {
+      $('#content').html('');
       $('#content').append(json.pets.map(displayPet));
     });
 }
 
 function displayPet(pet) {
-  return `<div><form><h3><a href="/egg-pet.html?pet=${pet.id}">${pet.name}</a></h3><button type="submit" class="delete button" >Delete</button></form></div>`;
+  return `
+  <div>
+    <form>
+      <h3>
+        <a href="/egg-pet.html?pet=${pet.id}">${pet.name}</a>
+      </h3>
+      <input class="pet-id" type="hidden" value="${pet.id}" />
+      <button type="submit" class="delete button" >Delete</button>
+    </form>
+  </div>`;
 }
 
 function deletePet(e) {
+  const form = $(e.target);
+  const petId = form.find('.pet-id').val();
   e.preventDefault();
-  fetch(`/pets/${pet.id}`, {
+  fetch(`/pets/${petId}`, {
       method: 'DELETE',
       headers: {
         'content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: $('input.name:checked').val(),
-        state: 'egg'
-      })
+      }
     })
+    .then(fetchPets)
     .catch(err => console.error(err));
   console.log("Deleting pet");
 }
