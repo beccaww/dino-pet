f3410a7f74ab2e4107181830bda58f7a6de78182
Rebecca Bob <rebecca.rupel@gmail.com>
2019-04-07 15:46:18 -0700

    minor changes

--- test/test-server.js
+++ test/test-server.js
@@ -4,7 +4,6 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const faker = require('faker');
 const mongoose = require('mongoose'); 
-//const app = require("../server.js");
 
 const should = chai.should(); 
 const expect = chai.expect;
